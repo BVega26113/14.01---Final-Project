cb6ffe11e107ebfc54eee023ccf92c11d243624e
Brendy <brendypedrazaveg@u.boisestate.edu>
2026-04-28 00:58:45 +0000

    Landing Page + NO design/styling

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
+/* GET home page - This sends your donut landing page */
+router.get('/', function(req, res, next) {
+  // This tells the server to send the index.html file from your public folder
+  res.sendFile(path.join(__dirname, '../public/MAIN_workspace/index.html'));
 });
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
-    try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
-        }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
-      });
-    } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
-    }
-});
-
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
-    });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
-    }
+/* GET comments page - You'll need this for Phase 2 later */
+router.get('/comments', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/MAIN_workspace/comments.html'));
 });
 
 module.exports = router;
