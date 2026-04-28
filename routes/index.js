ca2021cb48425573bd79c0a1ad9e3ded127248ae
Brendy <brendypedrazaveg@u.boisestate.edu>
2026-04-28 02:43:26 +0000

    FIXED navigation links (small fix)

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,21 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page - This sends your donut landing page */
 router.get('/', function(req, res, next) {
   // This tells the server to send the index.html file from your public folder
   res.sendFile(path.join(__dirname, '../public/MAIN_workspace/index.html'));
 });
 
-/* GET comments page - You'll need this for Phase 2 later */
 router.get('/comments', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/MAIN_workspace/comments.html'));
+});
+
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'menu.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 module.exports = router;
